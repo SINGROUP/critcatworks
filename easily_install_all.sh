5925dafc6dab055bb8e955610bf15a071f8d9d91
marchunter <marc.o.j.jaeger@gmail.com>
2019-03-07 16:04:00 +0200

    update readme

--- easily_install_all.sh
+++ easily_install_all.sh
@@ -1,7 +1,9 @@
 echo "Be sure you are in the critcatworks directory when starting this."
 echo "Do you have a new python3 virtual environment switched on?"
 echo ""
-echo "Automatically installing NOMAD parser, pycp2k, and critcatworks"
+echo "Automatically installing NOMAD parser, and critcatworks"
+
+pip install numpy cython
 
 echo "Installing NOMAD parser"
 echo "Installing NOMAD parser common"
@@ -13,21 +15,11 @@ echo "Installing NOMAD parser cp2k"
 cd ../parser-cp2k
 pip install -e .
 
-echo "pycp2k - input parser installation"
 echo "Installation under critcatworks/nomad_parser"
-
-cd ..
-git clone https://github.com/SINGROUP/pycp2k.git
-cd pycp2k
-python3 setup.py install
-2
-../cp2k4-1.xml
-3
-1
 
 echo "Installing critcatworks"
 
-cd ../../critcatworks
+cd ../..
 
 python3 setup.py install
 
