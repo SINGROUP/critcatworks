832799feefffe39c35454aea620ebad348368e9f
marc.o.j.jaeger <marc.o.j.jaeger@gmail.com>
2019-03-26 11:27:23 +0200

    coverage ladder concept

--- critcatworks/structure/coverage.py
+++ critcatworks/structure/coverage.py
@@ -326,6 +326,46 @@ class CoverageLadderTask(FiretaskBase):
 
     def run_task(self, fw_spec):
 
+        # ladder variables: l = number of minimum energies selected
+        #                   d = maximum depth of branches (convergence criterion)
+        #                   n0 = starting number of adsorbates
+        #                   structure0 = starting structure with adsorbates
+        
+
+
+        # DAG dictionary of jobs, simulation_ids
+
+        # dictionary of jobs per number of adsorbates (key = n, value = simulation_id)
+
+        # tracing variable how many adsorbates on the surface
+
+        # adsorption energy computation
+
+        # if root calculation
+        # decision to go up or down the ladder
+        # based on adsorption energy
+        # based on depth
+
+        # -------------------------------#
+
+        # similarity of occupied / unoccupied sites
+        # array of new structures with one adsorbate added / removed
+
+        # spawning of new jobs
+
+        # --------------------------------#
+
+        # select l minimum energy structures (l is fixed parameter) 
+        # comparing among energies of same number of adsorbates n
+        # if all in branch too high, continue on another branch
+        # otherwise, select as new root
+
+
+        # 
+
+
+
+
         update_spec.pop("_category")
         update_spec.pop("name")
         return FWAction(update_spec=update_spec) 
